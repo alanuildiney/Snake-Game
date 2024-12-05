163eb9b36eb2ba3a158d73e098bea6f08fd057d0
alanuildiney <62728529+alanuildiney@users.noreply.github.com>
2024-12-05 00:53:17 -0300

    Update script.js
    mudança da mensagem

--- javascript/script.js
+++ javascript/script.js
@@ -88,7 +88,7 @@ function isGameOver(){
     if(gameOver){
         ctx.fillStyle="white";
         ctx.font="50px verdana";
-        ctx.fillText("Game Over!", canvas.clientWidth/6.5, canvas.clientHeight/2);
+        ctx.fillText("simatei", canvas.clientWidth/6.5, canvas.clientHeight/2);
     }
 
     return gameOver;
